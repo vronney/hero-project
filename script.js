575ae5bc48bf182733bafe42087044090af51190
Ronney Vargas <ronneyvargas@gmail.com>
2019-08-05 20:52:59 -0500

    Added scollspy

--- script.js
+++ script.js
@@ -1,8 +1,39 @@
 $(document).ready(function() {
-  console.log('ready');
+
+  var topoffset = 55;
+  
+  // Use smooth scrolling when clicking on navigation
+  $('.navbar a[href*=\\#]:not([href=\\#])').click(function() {
+    if (location.pathname.replace(/^\//, '') === 
+    this.pathname.replace(/^\//, '') && 
+    location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - topoffset + 2
+        }, 500);
+        return false;
+      } //target.length
+    } //click function
+  }); //smooth scrolling
+
+  $(window).on('activate.bs.scrollspy', function(){
+    var hash = $('.site-nav')
+    .find('a.active')
+    .attr('href');
+
+    if (hash !== '#page-hero') {
+      $('header nav').addClass('inbody');
+    } else {
+      $('header nav').removeClass('inbody');
+    }
+  });
+
   $('#site-modal').on('show.bs.modal', function(event) {
     $(this)
       .find('.modal-content img')
       .attr('src', $(event.relatedTarget).data('highres'));
   });
+
 });
