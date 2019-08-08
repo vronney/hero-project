3b143b2cbcaabb7ace865360cb626512c88c8a4f
Ronney Vargas <ronneyvargas@gmail.com>
2019-08-07 19:37:28 -0500

    Added animation the site-header

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
     }
   });
 
-  $('#site-modal').on('show.bs.modal', function(event) {
+  $('#site-modal').on('show.bs.modal', function (event) {
     $(this)
       .find('.modal-content img')
       .attr('src', $(event.relatedTarget).data('highres'));
