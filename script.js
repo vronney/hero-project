33064ddf20bc4555a2b024f6cf934799d116bc74
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2019-08-09 14:38:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ $(document).ready(function() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-      .register('../service-worker.js')
+      .register('./service-worker.js')
       .then(reg => console.log('Service Worker: Registered'))
       .catch(err => console.log(`Service Worker: Error: ${err}`))
     });
