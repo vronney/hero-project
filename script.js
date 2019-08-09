584509bb727fbba6e6740b324f97c33474befddd
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2019-08-09 09:13:53 -0500

    Removed service worker from script file.

--- script.js
+++ script.js
@@ -51,19 +51,3 @@ $(document).ready(function() {
       .attr('src', $(event.relatedTarget).data('highres'));
   });
 });
-
-//Service Worker
-      if ('serviceWorker' in navigator) {
-
-        navigator.serviceWorker
-        .register('./service-worker.js', { scope: './' })
-        .then(function(registration) {
-        console.log("Service Worker Registered");
-      })
-        .catch(function(err) {
-        console.log("Service Worker Failed to Register", err);
-      })
-
-  }
-
-
