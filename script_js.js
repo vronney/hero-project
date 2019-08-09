e704de2fd33d61be44a1dcc9fe19989bad69d3bc
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2019-08-09 09:15:00 -0500

    Create script_js.js

---
+++ script_js.js
@@ -0,0 +1,13 @@
+//Service Worker
+      if ('serviceWorker' in navigator) {
+
+        navigator.serviceWorker
+        .register('./service-worker.js', { scope: './' })
+        .then(function(registration) {
+        console.log("Service Worker Registered");
+      })
+        .catch(function(err) {
+        console.log("Service Worker Failed to Register", err);
+      })
+
+  }
