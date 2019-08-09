926975a516219ce32a98d01ee7763f57b862d665
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2019-08-09 13:26:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -53,38 +53,45 @@
 // });
 
 
-    //Service Worker
-      if ('serviceWorker' in navigator) {
+//Service Worker
+//Check if service worker is supported
+  if ('serviceWorker' in navigator) {
+    console.log('Service Worker is Supported');
+  
+  }
 
-        navigator.serviceWorker
-        .register('~service-worker.js', { scope: '/' })
-        .then(function(registration) {
-        console.log("Service Worker Registered");
-      })
-        .catch(function(err) {
-        console.log("Service Worker Failed to Register", err);
-      })
 
-    }
-    // Function to perform HTTP request
-  var get = function(url) {
-  return new Promise(function(resolve, reject) {
+//       if ('serviceWorker' in navigator) {
 
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                var result = xhr.responseText
-                result = JSON.parse(result);
-                resolve(result);
-            } else {
-                reject(xhr);
-            }
-        }
-    };
+//         navigator.serviceWorker
+//         .register('~service-worker.js', { scope: '/' })
+//         .then(function(registration) {
+//         console.log("Service Worker Registered");
+//       })
+//         .catch(function(err) {
+//         console.log("Service Worker Failed to Register", err);
+//       })
+
+//     }
+//     // Function to perform HTTP request
+//   var get = function(url) {
+//   return new Promise(function(resolve, reject) {
+
+//     var xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function() {
+//         if (xhr.readyState === XMLHttpRequest.DONE) {
+//             if (xhr.status === 200) {
+//                 var result = xhr.responseText
+//                 result = JSON.parse(result);
+//                 resolve(result);
+//             } else {
+//                 reject(xhr);
+//             }
+//         }
+//     };
     
-    xhr.open("GET", url, true);
-    xhr.send();
+//     xhr.open("GET", url, true);
+//     xhr.send();
 
-  }); 
-};
+//   }); 
+// };
