e2fddc7cb7815198e7b676bc16c192f0c6f7a41e
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2019-08-09 10:14:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+//Service Worker
+      if ('serviceWorker' in navigator) {
+
+        navigator.serviceWorker
+        .register('service-worker.js', { scope: '/' })
+        .then(function(registration) {
+        console.log("Service Worker Registered");
+      })
+        .catch(function(err) {
+        console.log("Service Worker Failed to Register", err);
+      })
+
+  }
+
 $(document).ready(function() {
 
   var topoffset = 55;
