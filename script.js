e430f6446c23a455dd18cbb0f0dd828e920841ad
Ronney Vargas <37166515+vronney@users.noreply.github.com>
2019-08-09 14:52:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -63,39 +63,3 @@ $(document).ready(function() {
       .catch(err => console.log(`Service Worker: Error: ${err}`))
     });
   }
-
-
-//       if ('serviceWorker' in navigator) {
-
-//         navigator.serviceWorker
-//         .register('~service-worker.js', { scope: '/' })
-//         .then(function(registration) {
-//         console.log("Service Worker Registered");
-//       })
-//         .catch(function(err) {
-//         console.log("Service Worker Failed to Register", err);
-//       })
-
-//     }
-//     // Function to perform HTTP request
-//   var get = function(url) {
-//   return new Promise(function(resolve, reject) {
-
-//     var xhr = new XMLHttpRequest();
-//     xhr.onreadystatechange = function() {
-//         if (xhr.readyState === XMLHttpRequest.DONE) {
-//             if (xhr.status === 200) {
-//                 var result = xhr.responseText
-//                 result = JSON.parse(result);
-//                 resolve(result);
-//             } else {
-//                 reject(xhr);
-//             }
-//         }
-//     };
-    
-//     xhr.open("GET", url, true);
-//     xhr.send();
-
-//   }); 
-// };
